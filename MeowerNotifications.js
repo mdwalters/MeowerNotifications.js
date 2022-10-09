c35a1a6f74a8066b1603d4832ae6b9d86b8d5c03
M.D. Walters <808426@pdsb.net>
2022-10-09 17:14:58 -0400

    Bridge support + me begging someone to post

--- MeowerNotifications.js
+++ MeowerNotifications.js
@@ -6,7 +6,11 @@ function newNotification(body, icon, title) {
 }
 
 window.handlePost = async function(bundle) {
+    if (bundle[0] == "Discord" || bundle[0] == "Webhooks") {
+        var bundle = bundle[1].split(": ");
+    }
+
     var user = await fetchURL(`https://api.meower.org/users/${bundle[0]}`);
     var user = JSON.parse(user);
-    newNotification(bundle[1], `https://raw.githubusercontent.com/meower-media-co/Meower-Svelte/master/src/assets/avatars/icon_${(user.pfp_data - 1)}.svg`, bundle[0]);
+    newNotification(bundle[1], `https://raw.githubusercontent.com/BetterMeower-Project/BetterMeower/1.4.0/src/assets/avatars/icon_${(user.pfp_data - 1)}.svg`, bundle[0]);
 }
