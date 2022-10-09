7d1f74082db492ce19bf880d5fd736bef58f8c40
M.D. Walters <808426@pdsb.net>
2022-10-09 16:29:06 -0400

    Implement the real notification magic!

--- MeowerNotifications.js
+++ MeowerNotifications.js
@@ -1,5 +1,12 @@
-let notification = Notification.requestPermission();
+let notificationRequest = Notification.requestPermission();
+newNotification("MeowerNotifications is ready!", "https://raw.githubusercontent.com/meower-media-co/Meower-Svelte/master/src/assets/avatars/icon_21.svg", "MeowerNotifications");
 
-window.handlePost = function(bundle) {
-  
+function newNotification(body, icon, title) {
+    const notification = new Notification(title, { body, icon });
+}
+
+window.handlePost = async function(bundle) {
+    var user = await fetchURL(`https://api.meower.org/users/${bundle[0]}`);
+    var user = JSON.parse(user);
+    newNotification(bundle[1], `https://raw.githubusercontent.com/meower-media-co/Meower-Svelte/master/src/assets/avatars/icon_${(user.pfp_data - 1)}.svg`, bundle[0]);
 }
